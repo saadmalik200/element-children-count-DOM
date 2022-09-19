c728cd847c251290df12f1d2e2cfe53fb1159117
Saad Malik <saad_malik200@hotmail.com>
2022-09-19 15:24:15 +0200

    ex3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,28 @@
 const wrapper = document.getElementById("wrapper");
-console.log(wrapper.children);
-console.log(Array.from(wrapper.children).length);
+// console.log(wrapper.children);
+
+console.log(`--------------------------------`);
+
+console.log(wrapper.children.length);
+console.log(wrapper.childElementCount);
+
+console.log(`--------------------------------`);
+
 Array.from(wrapper.children).forEach((item) => {
-  console.log(item);
+  console.log(item.nodeName);
 });
 
-Array.from(wrapper.children).forEach((item) => console.log(item.children));
+console.log(`--------------------------------`);
+
+Array.from(wrapper.children).forEach((item) => {
+  //   console.log(item);
+  console.log("The length is: ", item.children.length);
+  Array.from(item.children).forEach((el, i) => {
+    console.log(el.nodeName);
+
+    // console.log(el);
+  });
+});
 
 console.log(`-----------------------------------------`);
 //////////////
@@ -19,12 +36,13 @@ function checkLength() {
     result += item.classList.length;
   });
   Array.from(wrapper.children).forEach((el) => {
-    console.log(el.classList.length);
+    // console.log(el.classList.length);
     element.push(el.classList.length);
+    // console.log(element);
     element.sort((a, b) => b - a);
     maxNum = element[0];
   });
-  console.log(maxNum);
+  //   console.log(maxNum);
   obj.mostClasses = maxNum;
   obj.totalClasses = result;
 
